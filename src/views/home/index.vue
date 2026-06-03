b4013f37015bb30f491f760a235f7759d98b5db5
lkl <1691487896@qq.com>
2026-06-03 21:03:57 +0800

    feat: 初始化 Vue3 + TS 大事件项目

---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <template v-for="item in items" :key="item.id">
+    <div v-if="item.state">
+      {{ item.name }}
+    </div>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const items = ref([
+  { name: "3mz", id: 1, state: true },
+  { name: "pite", id: 2, state: true },
+  { name: "roieee", id: 3, state: false },
+]);
+</script>
+
+<style scoped></style>

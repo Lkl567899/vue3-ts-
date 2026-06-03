10af28ad403e8cab31fd72ce6aae9d1f2d25e2df
lkl <1691487896@qq.com>
2026-06-03 22:04:39 +0800

    feat: 完善 main.ts 中的 pinia 持久化配置

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import persist from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(persist)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')

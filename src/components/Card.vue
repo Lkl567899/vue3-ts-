070701b118ffa8604c8c9a9e883f5fcac2fa1820
lkl <1691487896@qq.com>
2026-06-09 03:07:30 +0800

    feat:配置了layout架子  新增文章分类页面 封装了公用的card组件

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-card>
+        <template #header>
+            <div class="header">
+                <span>{{ title }}</span>
+                <div class="extra">
+                    <slot name="extra"></slot>
+                </div>
+            </div>
+        </template>
+        <!-- 默认插槽 -->
+        <slot></slot>
+    </el-card>
+</template>
+
+<script setup lang="ts">
+type Props = {
+    title: string
+}
+const props = defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+.el-card {
+    min-height: 100%;
+    box-sizing: border-box;
+
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+}
+</style>
---
+++ src/views/channel/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <Card :title="'文章管理'">
+        <template #extra><el-button type="primary">添加分类</el-button></template>
+        123
+    </Card>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss"></style>
